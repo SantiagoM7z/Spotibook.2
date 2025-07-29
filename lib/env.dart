d450c2a2b6e373f5102a6126f328b4f9f6ae31f0
damian-pebe <damian.pebe@gmail.com>
2025-07-28 23:51:17 -0600

    reemove env.dart from tracking and add to .gitignore

--- lib/env.dart
+++
@@ -1,4 +0,0 @@
-const String stripePublishableKey =
-    'pk_test_51PzNMZJuhgKcHeSBQsFZ0xyefw3pwW6zOK6p5ZBdKcgCl8B3Y0ygxXw5NmrCzEewve1wiVxXOmonp9qe9RekLSVO009lpXCRcK';
-const String stripeSecretKey =
-    'sk_test_51PzNMZJuhgKcHeSBjQDA341v595VslSvDsi0EAQQOLf0icKHaV3uh6msY1lXqvPdtKNXnyGWmgmSxC6u3ozdSpQ600W9H5ajw9';
